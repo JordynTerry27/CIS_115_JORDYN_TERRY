1a167da008375145f5008185e7e2307af8df4d24
SANDHILLS\LH218-24 <jad.talbert@gmail.com>
2026-08-27 10:17:38 -0400

    done

---
+++ Week2Labs/divide_by_zero.py
@@ -0,0 +1,7 @@
+num1 = input("Enter number: ")
+
+divider = 0
+
+result = num1 / divider
+
+print(result)
---
+++ Week2Labs/product_two_numbers.py
@@ -0,0 +1,7 @@
+
+num1 = input("Enter the first number: ")
+num2 = input("Enter the second number: ")
+
+result = int(num1)  * int(num2)
+
+print(f'result')
