6e6c1d84a752f191beaf7e6e67d31271776f62b5
SANDHILLS\LH218-24 <jad.talbert@gmail.com>
2026-09-03 09:43:16 -0400

    done

---
+++ Week3Labs/sum_difference.py
@@ -0,0 +1,9 @@
+num1 = input("Enter first number: ")
+num2 = input("Enter second number: ")
+num3 = input("Enter third number: ")
+
+sum = float(num1) + float(num2) + float(num3)
+print(f"{sum} is the sum.")
+
+difference = (float(num1) - float(num2)) + float(num3)
+print(f"{difference} is the difference.")
