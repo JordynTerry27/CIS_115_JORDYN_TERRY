41a96210d0f3fbe8a58d1c471843b7beaa338674
SANDHILLS\LH218-24 <jad.talbert@gmail.com>
2026-09-17 09:11:02 -0400

    done

--- Week4Labs/item_seperator.py
+++ Week4Labs/item_seperator.py
@@ -1,1 +1,7 @@
-print
+street_address = input("Enter your street adress: ")
+city = input("Enter city: " )
+state = input("Enter state: ")
+zip = input("Enter ZIP code: ")
+country = input("Enter country: ")
+
+print(f"{street_address}\n{city}, {state} {zip}\n{country}")
