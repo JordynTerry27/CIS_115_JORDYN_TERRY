f02cbdf33dd3cb3b19b7f305043e96ee88c3033a
SANDHILLS\LH218-24 <jad.talbert@gmail.com>
2026-09-22 09:50:15 -0400

    nowhere near

---
+++ Week6Labs/pythagorean_theorem.py
@@ -0,0 +1,6 @@
+import math
+x = float(input("Enter a value for x: "))
+y = float(input("Enter a value for y: "))
+
+def hyp(x,y):
+    c =  
---
+++ Week6Labs/two_message.py
@@ -0,0 +1,10 @@
+def print_message1():
+    print("I was called first")
+    print_message2()
+    return
+
+def print_message2():
+    print("I was called from print_message1()")
+    return
+
+print_message1()
